8e6daa3b464036706385ad9a483b0f04c30eff03
innomerve <murielleslokonon@gmail.com>
2023-05-01 00:10:04 +0200

    Trying to solve cors origin problem

--- src/main/java/jpa/responses/AppResponse.java
+++ src/main/java/jpa/responses/AppResponse.java
@@ -11,23 +11,11 @@ public class AppResponse {
         json.put("message", message);
 
         return Response.status(status)
-        .header("Access-Control-Allow-Origin", "*")
-        .header("Access-Control-Allow-Credentials", "true")
-        .header("Access-Control-Allow-Headers",
-            "origin, content-type, accept, authorization")
-        .header("Access-Control-Allow-Methods", 
-            "GET, POST, PUT, DELETE, OPTIONS, HEAD")
         .entity(json.toString()).type(MediaType.APPLICATION_JSON).build();
     }
 
     public static Response success(Object o){
         return Response.status(Response.Status.OK)
-        .header("Access-Control-Allow-Origin", "*")
-        .header("Access-Control-Allow-Credentials", "true")
-        .header("Access-Control-Allow-Headers",
-            "origin, content-type, accept, authorization")
-        .header("Access-Control-Allow-Methods", 
-            "GET, POST, PUT, DELETE, OPTIONS, HEAD")
         .entity(o)
         .type(MediaType.APPLICATION_JSON).build();
     }
